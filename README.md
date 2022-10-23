1cbba8012d938bbc490a972cab6ea7009eae8af1
stew <s.c.mcdowall@cml.leidenuniv.nl>
2022-10-23 19:50:33 +0200

    first upload

---
+++ README.md
@@ -0,0 +1,19 @@
+
+## Welk lidwoord heeft dat naamwoord?
+
+### Which article does that Dutch noun have?
+
+A short bash script to return a given Dutch word along with its article and english translation
+
+Created because I could not find a CLI translator or dictionary that would give me what I needed (without double translating and adding "the", which sometimes returns a synonym)
+
+### Requirements
+
+Uses [curl](http://curl.haxx.se/) to pull from [welklidwoord.nl](http://welklidwoord.nl) and [grep](https://www.gnu.org/software/grep/) to extract the relevant text from the html
+
+### Usage
+
+   `bash welk-lidwoord.sh <WORD>`
+
+
+
---
+++ welk_lidwoord.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+# calls the online dictionary and pulls relevant text from the html response
+html=$(curl -s https://www.welklidwoord.nl/{$1})
+
+nl=$(echo "$html" | grep -oP '(?<=(gebruiken wij )).*')
+en=$(echo "$html" | grep -oP '(?<=(English: </strong>)).*?(?=<)')
+fr=$(echo "$html" | grep -oP '(?<=(Français: </strong>)).*?(?=<)')
+de=$(echo "$html" | grep -oP '(?<=(Deutsch: </strong>)).*?(?=<)')
+
+# remove pipe or dot at end
+nl=$(echo ${nl//.})
+# some german and french words are produced in the html with the wrong article, remove from all
+fr=$(echo ${fr//|} | cut -d " " -f-2)
+de=$(echo ${de//|} | cut -d " " -f-2)
+
+# define formatting
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+BOLD=$(tput bold)
+NORM=$(tput sgr0)
+
+# prints dutch word with article, along with the other translations available in the html
+echo -e "* Dutch: ${RED}${BOLD}${nl}${NC}${NORM}"
+echo "* English: "$en
+echo "* German: "$de
+echo "* French: "$fr
+
+
